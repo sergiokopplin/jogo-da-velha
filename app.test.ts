f45e08f5538a923535d7c6299e2e96f873d4c465
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-15 19:00:46 -0300

    chore: adds some docs

--- app.test.ts
+++ app.test.ts
@@ -2,7 +2,7 @@ import { TicTacToe } from './app'
 
 describe('TicTacToe', () => {
   test('Should construct ok', () => {
-    const makeGame = new TicTacToe([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
+    const makeGame = new TicTacToe([[0, 1, 0], [2, 0, 2], [1, 2, 1]])
     expect(makeGame).toBeTruthy()
   })
 
@@ -10,6 +10,6 @@ describe('TicTacToe', () => {
     expect(() => new TicTacToe([])).toThrowError('Invalid Board')
     expect(() => new TicTacToe([[], [], []])).toThrowError('Invalid Board')
     expect(() => new TicTacToe([[1], [2], [3]])).toThrowError('Invalid Board')
-    expect(() => new TicTacToe([[2, 2, 2], [3, 3, 3], [4, 4, 4]])).toThrowError('Invalid Board')
+    expect(() => new TicTacToe([[5, 5, 5], [3, 3, 3], [4, 4, 4]])).toThrowError('Invalid Board')
   })
 })
--- app.ts
+++ app.ts
@@ -4,11 +4,13 @@ export class TicTacToe {
   private readonly board: Board;
   private readonly rows: Number;
   private readonly columns: Number;
+  private readonly validValues: Number[]
 
   constructor (board: Board) {
     this.board = board
     this.columns = 3
     this.rows = 3
+    this.validValues = [0, 1, 2]
 
     if (!this.isValidBoard()) {
       throw new Error('Invalid Board')
@@ -24,10 +26,16 @@ export class TicTacToe {
       const hasNotEnoughItems = column.length !== this.rows
       if (hasNotEnoughItems) return true
 
-      const hasDifferentNumbers = column.some(item => item !== 1 && item !== 0)
+      const hasDifferentNumbers = column.some(item => !this.validValues.includes(item))
       if (hasDifferentNumbers) return true
     })
 
     return !hasError
   }
+
+  // Status: empty | running | tie | winner
+  // Winner: null | 1 | 2
+
+  // getStatus(board): status
+  // getWinner(board): Winner
 }
