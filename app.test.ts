07534b784b92a900e7ea8bec5e19ecd7769be74a
Sergio Kopplin <sergiokopplin@gmail.com>
2022-06-15 20:54:46 -0300

    feat: refactor and calculate winner

--- app.test.ts
+++ app.test.ts
@@ -2,37 +2,58 @@ import { TicTacToe } from './app'
 
 describe('TicTacToe', () => {
   test('Should construct ok', () => {
-    const makeGame = new TicTacToe([[0, 1, 0], [2, 0, 2], [1, 2, 1]])
+    const makeGame = new TicTacToe([0, 1, 0, 2, 0, 2, 1, 2, 1])
     expect(makeGame).toBeTruthy()
   })
 
-  test('Should return Status Running properly', () => {
-    expect(new TicTacToe([[1, 0, 0], [0, 0, 0], [0, 0, 0]]).getStatus()).toBe('running')
-    expect(new TicTacToe([[1, 1, 0], [2, 2, 0], [1, 2, 0]]).getStatus()).toBe('running')
-  })
+  test.only('Should return Winner properly', () => {
+    const games = {
+      rows: [
+        [1, 1, 1, 2, 2, 0, 0, 0, 0],
+        [2, 2, 0, 1, 1, 1, 0, 0, 0],
+        [2, 2, 0, 0, 0, 0, 1, 1, 1]
+      ],
+      columns: [
+        [1, 2, 0, 1, 2, 0, 1, 0, 0],
+        [2, 1, 0, 2, 1, 0, 0, 1, 0],
+        [0, 0, 1, 0, 2, 1, 0, 2, 1]
+      ],
+      diagonals: [
+        [1, 2, 0, 2, 1, 0, 2, 0, 1],
+        [2, 0, 1, 2, 1, 0, 1, 2, 0]
+      ]
+    }
 
-  // Errors
+    games.rows.forEach(game => {
+      expect(new TicTacToe(game).getWinner()).toBe(1)
+    })
+    games.columns.forEach(game => {
+      expect(new TicTacToe(game).getWinner()).toBe(1)
+    })
+    games.diagonals.forEach(game => {
+      expect(new TicTacToe(game).getWinner()).toBe(1)
+    })
+  })
 
   test('Should throw error when Empty arrays passed', () => {
     expect(() => new TicTacToe([])).toThrowError('Invalid Board')
-    expect(() => new TicTacToe([[], [], []])).toThrowError('Invalid Board')
   })
 
   test('Should throw error when Not Enough Numbers passed', () => {
-    expect(() => new TicTacToe([[1], [2], [3]])).toThrowError('Invalid Board')
+    expect(() => new TicTacToe([1, 2, 3])).toThrowError('Invalid Board')
   })
 
   test('Should throw error when Not Valid Numbers passed', () => {
-    expect(() => new TicTacToe([[5, 5, 5], [3, 3, 3], [4, 4, 4]])).toThrowError('Invalid Board')
+    expect(() => new TicTacToe([5, 5, 5, 3, 3, 3, 4, 4, 4])).toThrowError('Invalid Board')
   })
 
   test('Should throw error when Only Zeros passed', () => {
-    expect(() => new TicTacToe([[0, 0, 0], [0, 0, 0], [0, 0, 0]])).toThrowError('Invalid Board')
+    expect(() => new TicTacToe([0, 0, 0, 0, 0, 0, 0, 0, 0])).toThrowError('Invalid Board')
   })
 
   test('Should throw error when Invalid Moves passed', () => {
-    expect(() => new TicTacToe([[1, 1, 0], [0, 0, 0], [0, 0, 0]])).toThrowError('Invalid Board')
-    expect(() => new TicTacToe([[1, 1, 1], [2, 0, 0], [0, 0, 0]])).toThrowError('Invalid Board')
-    expect(() => new TicTacToe([[1, 1, 1], [2, 2, 2], [1, 1, 1]])).toThrowError('Invalid Board')
+    expect(() => new TicTacToe([1, 1, 0, 0, 0, 0, 0, 0, 0])).toThrowError('Invalid Board')
+    expect(() => new TicTacToe([1, 1, 1, 2, 0, 0, 0, 0, 0])).toThrowError('Invalid Board')
+    expect(() => new TicTacToe([1, 1, 1, 2, 2, 2, 1, 1, 1])).toThrowError('Invalid Board')
   })
 })
--- app.ts
+++ app.ts
@@ -1,14 +1,11 @@
-type Board = number[][]
-type Status = 'running' | 'tie' | 'winner'
+type Board = number[]
 
 export class TicTacToe {
   private readonly board: Board
-  private readonly dimensions: number
   private readonly validValues: number[]
 
   constructor (board: Board) {
     this.board = board
-    this.dimensions = 3
     this.validValues = [0, 1, 2]
 
     if (!this.isValidBoard()) {
@@ -17,29 +14,27 @@ export class TicTacToe {
   }
 
   private hasEnoughItems (): boolean {
-    return this.board.some(row => row.length === this.dimensions)
+    return this.board.length === 9
   }
 
   private hasOnlyAllowedItems (): boolean {
-    return this.board.some(row => row.some(item => this.validValues.includes(item)))
+    return this.board.some(item => this.validValues.includes(item))
   }
 
   private hasNotOnlyZeros (): boolean {
-    return this.board.some(row => row.some(item => [1, 2].includes(item)))
+    return this.board.some(item => [1, 2].includes(item))
   }
 
   private hasOnlyValidMoves (): boolean {
     let countFirstPlayer = 0
     let countSecondPlayer = 0
 
-    this.board.forEach(row => {
-      row.forEach(item => {
-        if (item === 1) {
-          countFirstPlayer++
-        } else if (item === 2) {
-          countSecondPlayer++
-        }
-      })
+    this.board.forEach(item => {
+      if (item === 1) {
+        countFirstPlayer++
+      } else if (item === 2) {
+        countSecondPlayer++
+      }
     })
 
     return Math.abs(countFirstPlayer - countSecondPlayer) <= 1
@@ -56,9 +51,34 @@ export class TicTacToe {
     return validations.filter(item => item === true).length === validations.length
   }
 
-  // Status: running | tie | winner
-  // Winner: null | 1 | 2
+  private calculateWinner (p1: number, p2: number, p3: number): number | null {
+    if (this.board[p1] === 0 || this.board[p2] === 0 || this.board[p3] === 0) {
+      return null
+    }
 
-  // getStatus(board): status
-  // getWinner(board): Winner
+    const winner = this.board[p1] === this.board[p2] && this.board[p2] === this.board[p3]
+
+    if (winner) {
+      return this.board[p1]
+    }
+
+    return null
+  }
+
+  public getWinner (): number | null {
+    const winner =
+      // rows
+      this.calculateWinner(0, 1, 2) ||
+      this.calculateWinner(3, 4, 5) ||
+      this.calculateWinner(6, 7, 8) ||
+      // columns
+      this.calculateWinner(0, 3, 6) ||
+      this.calculateWinner(1, 4, 7) ||
+      this.calculateWinner(2, 5, 8) ||
+      // diagonals
+      this.calculateWinner(0, 4, 8) ||
+      this.calculateWinner(2, 4, 6)
+
+    return winner
+  }
 }
